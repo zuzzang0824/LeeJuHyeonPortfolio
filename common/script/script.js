5adf3bb4c5826953627d4b86c2ae195d2c219b8d
zuzzang0824 <tiny0824@naver.com>
2023-09-04 17:25:35 +0900

    230904

--- common/script/script.js
+++ common/script/script.js
@@ -1,3 +1,6 @@
+AOS.init();
+
+// swiper
 new Swiper(".swiper", {
   autoplay: {
     delay: 2500,
@@ -13,3 +16,62 @@ new Swiper(".swiper", {
     1025: { slidesPerView: 3 },
   },
 });
+
+// profile
+$(".menuBtn").on("click", () => {
+  $(".mobileGNBdrop").show(300, "swing");
+
+  $(".menuBtn").hide();
+  $(".menuClose").show();
+});
+
+$(".menuClose").on("click", () => {
+  $(".mobileGNBdrop").hide(300, "swing");
+  $(".menuBtn").show();
+  $(".menuClose").hide();
+});
+
+// topBtn
+$(window).scroll(function () {
+  let top = $(window).scrollTop();
+  if (top > 1500) {
+    $(".topBtn").fadeIn();
+  } else {
+    $(".topBtn").fadeOut();
+  }
+});
+
+// expandInfo
+$(".expandArrow").click(function (event) {
+  event.preventDefault();
+  $(".expandInfo").slideDown(300, "swing");
+});
+$(".scrollClose").click(function () {
+  $(".expandInfo").slideUp(300, "swing");
+});
+
+// mouse event
+let mouseX = 0;
+let mouseY = 0;
+
+let cursorX = 0;
+let cursorY = 0;
+
+const cursor = document.querySelector(".cursor");
+
+document.body.addEventListener("mousemove", (e) => {
+  mouseX = e.clientX;
+  mouseY = e.clientY;
+});
+
+function mouseEffect() {
+  requestAnimationFrame(mouseEffect);
+
+  cursorX += (mouseX - cursorX) * 0.1;
+  cursorY += (mouseY - cursorY) * 0.1;
+
+  cursor.style.transform = `translate(${cursorX}px,
+    ${cursorY}px)`;
+}
+
+mouseEffect();
